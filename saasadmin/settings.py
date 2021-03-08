e6c9ecc245baa5350a0f8598d30167703f976eaa
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-03-08 21:25:38 +0100

    add initial model. add apps to settings file ./manage.py makemigrations

--- saasadmin/settings.py
+++ saasadmin/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.backend',
+    'apps.api_customer',
+    'apps.api_admin',
 ]
 
 MIDDLEWARE = [
