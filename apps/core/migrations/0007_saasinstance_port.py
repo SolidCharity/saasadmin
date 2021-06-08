376443a256d98e9a2d4f81d77461414410de23b1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-06-08 19:48:23 +0200

    fix migration

--- apps/core/migrations/0007_saasinstance_port.py
+++ apps/core/migrations/0007_saasinstance_port.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0005_saasinstance_initial_password'),
+        ('core', '0006_auto_20210525_2002'),
     ]
 
     operations = [
