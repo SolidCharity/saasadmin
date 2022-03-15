00454f0c30c1e51769c49eed5f93055582d0fbcc
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-15 22:56:44 +0100

    fix the names of the sequences

--- demodata/insertdemo.psql.sql
+++ demodata/insertdemo.psql.sql
@@ -1,7 +1,7 @@
 -- required for postgresql
 ALTER SEQUENCE auth_user_id_seq RESTART WITH 10;
-ALTER SEQUENCE "core_saasproduct_id_seq" RESTART WITH 10;
-ALTER SEQUENCE "plan_id_seq" RESTART WITH 10;
-ALTER SEQUENCE "Customer_id_seq" RESTART WITH 10;
-ALTER SEQUENCE "instance_id_seq" RESTART WITH 10;
-ALTER SEQUENCE "contract_id_seq" RESTART WITH 10;
+ALTER SEQUENCE "saas_product_id_seq" RESTART WITH 10;
+ALTER SEQUENCE "saas_plan_id_seq" RESTART WITH 10;
+ALTER SEQUENCE "saas_customer_id_seq" RESTART WITH 10;
+ALTER SEQUENCE "saas_instance_id_seq" RESTART WITH 10;
+ALTER SEQUENCE "saas_contract_id_seq" RESTART WITH 10;
