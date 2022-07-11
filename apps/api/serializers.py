20287786aadcda11252004d3fbb0c8932116c526
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-07-11 22:27:59 +0200

    consider dbms_type for running Ansible scripts

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -29,4 +29,4 @@ class InstanceSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = SaasInstance
-        fields = ('identifier', 'hostname', 'pacuser', 'activation_token', 'db_password', 'initial_password', 'first_port', 'last_port', 'status', 'instance_url', 'prefix', 'password1', 'password2', 'django_secret_key', 'quota_storage', 'quota_app', 'custom_domain')
+        fields = ('identifier', 'hostname', 'pacuser', 'activation_token', 'db_password', 'initial_password', 'first_port', 'last_port', 'status', 'instance_url', 'prefix', 'password1', 'password2', 'django_secret_key', 'quota_storage', 'quota_app', 'custom_domain', 'dbms_type')
